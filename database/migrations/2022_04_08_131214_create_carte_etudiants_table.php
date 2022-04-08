055e15318ed5eb70929dbad01a68c9cdae5f0b26
@Momo_D <mouhamadoud34@gmail.com>
2022-04-08 15:53:08 +0200

    Debut projet laravel niveau 4.e

---
+++ database/migrations/2022_04_08_131214_create_carte_etudiants_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarteEtudiantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carte_etudiants', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carte_etudiants');
+    }
+}
